83eb19f3f6042832cdd6c0d34bd507c13726a746
Luke <40407039@qub.ac.uk>
2026-01-04 18:36:31 +0000

    basic User.java set up for users entity

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/model/User.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/model/User.java
@@ -1,5 +1,26 @@
 package com.cruxconqueror.crux_conqueror.model;
+import java.time.LocalDateTime;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
     
+@Id
+@Column(name = "User_ID")
+private Long id;
+
+@Column(name = "Usernames", nullable = false, unique = true)
+private String username;
+
+@Column(name = "Emails", nullable =false, unique = true)
+private String email;
+
+@Column(name = "Password_Hash", nullable = false, unique = true)
+private String passwordHash;
+
+@Column(name = "Date_Made")
+private LocalDateTime dateMade;
+
 }
