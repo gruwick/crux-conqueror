bb52554eca4f8074f689f9e51a87107649b57aa6
Luke <40407039@qub.ac.uk>
2026-01-04 16:49:47 +0000

    Connected my database to my JavaSpring boot and created a HomeController so that i can see the backend is working instead of just errors

---
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/HomeController.java
@@ -0,0 +1,12 @@
+package com.cruxconqueror.crux_conqueror;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Crux Conqueror's backend is up and running";
+    }
+    
+}
