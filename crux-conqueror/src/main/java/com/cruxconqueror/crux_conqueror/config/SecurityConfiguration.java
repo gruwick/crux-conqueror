45c4874dea4c7dfcc2f6cc741e106e428083a70c
Luke <40407039@qub.ac.uk>
2026-01-06 19:25:41 +0000

    Updating my Security Cinfig so that i can test (/test) endpoints without having to login each time

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/config/SecurityConfiguration.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/config/SecurityConfiguration.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
+
+
 
 @Configuration
 public class SecurityConfiguration {
@@ -11,5 +16,17 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+    @Bean 
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/test/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .formLogin(Customizer.withDefaults());
+
+    return http.build();
     }
 }
