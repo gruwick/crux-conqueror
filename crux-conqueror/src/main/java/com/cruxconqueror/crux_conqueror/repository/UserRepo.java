13d9c661cdfd4f22d4e56c1eff7ea3d496e1acf0
Luke <40407039@qub.ac.uk>
2026-01-06 21:50:06 +0000

    Working towards implementing login/logout endpoints

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/UserRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/UserRepo.java
@@ -1,7 +1,8 @@
 package com.cruxconqueror.crux_conqueror.repository;
 import com.cruxconqueror.crux_conqueror.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User, Long>{
-    
+    Optional<User> findByUsername(String username);
 }
