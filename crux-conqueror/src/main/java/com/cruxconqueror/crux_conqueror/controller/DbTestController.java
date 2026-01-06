c4e425e1ace52e21bb82f6340b1f53aed4eb309f
Luke <40407039@qub.ac.uk>
2026-01-06 17:28:21 +0000

    Reformating and removing old test code as well as saving hashed passwords to the database instead of raw

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/controller/DbTestController.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/controller/DbTestController.java
@@ -1,32 +1,37 @@
 package com.cruxconqueror.crux_conqueror.controller;
 import com.cruxconqueror.crux_conqueror.repository.UserRepo;
 import com.cruxconqueror.crux_conqueror.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.List;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/test")
 public class DbTestController {
-private final UserRepo userRepo;
 
-public DbTestController(UserRepo userRepo){
+private final UserRepo userRepo;
+private final PasswordEncoder passwordEncoder;
+
+public DbTestController(UserRepo userRepo, PasswordEncoder passwordEncoder){
     this.userRepo = userRepo;
+    this.passwordEncoder = passwordEncoder;
 }
+
 @GetMapping("/users")
 public List<User> users() {
     return userRepo.findAll();
 }
+
 @GetMapping("/create")
 public User createUser(
     @RequestParam String username,
     @RequestParam String email,
     @RequestParam String password
 ) {
+    String hashedPassword = passwordEncoder.encode(password);
     //Reminder to myself to remove this after testing, not needed long term
-    User u = new User(username, email, password);
+    //Storing the hashed password now and not the raw
+    User u = new User(username, email, hashedPassword);
     return userRepo.save(u);
 }
 }
