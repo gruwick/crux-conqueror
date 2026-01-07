4c570e020e7dca0646b23ab762cd84ab67143873
Luke <40407039@qub.ac.uk>
2026-01-07 23:54:09 +0000

    updating my old homeController so that it can be linked to a template

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/controller/HomeController.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.cruxconqueror.crux_conqueror.controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "Crux Conqueror's backend is up and running";
+        return "home";
     }
     
 }
