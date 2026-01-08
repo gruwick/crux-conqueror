1e682afd7215a97381d64ccd63a188eeef2e7944
Luke <40407039@qub.ac.uk>
2026-01-08 14:47:58 +0000

    Creating my TrainingSessionsRepo so that Sessions can be saved to users

---
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/TrainingSessionsRepo.java
@@ -0,0 +1,9 @@
+package com.cruxconqueror.crux_conqueror.repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cruxconqueror.crux_conqueror.model.TrainingSessions;
+import com.cruxconqueror.crux_conqueror.model.User;
+
+public interface TrainingSessionsRepo extends JpaRepository<TrainingSessions, Long> {
+    List<TrainingSessions> findByUserOrderBySessionDateDesc(User user)
+}
