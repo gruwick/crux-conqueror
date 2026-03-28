ef2195e332fe4a346da8ecdda165720288b75413
Luke <40407039@qub.ac.uk>
2026-03-28 17:26:03 +0000

    Populated a basic repo for nutrition

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/FoodEntryRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/FoodEntryRepo.java
@@ -1,5 +1,10 @@
 package com.cruxconqueror.crux_conqueror.repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cruxconqueror.crux_conqueror.model.FoodEntry;
+import com.cruxconqueror.crux_conqueror.model.User;
 
-public class FoodEntryRepo {
+public interface FoodEntryRepo extends JpaRepository<FoodEntry, Long> {
+    List<FoodEntry> findByUserOrderByEntryDateTimeDesc(User user);
     
 }
