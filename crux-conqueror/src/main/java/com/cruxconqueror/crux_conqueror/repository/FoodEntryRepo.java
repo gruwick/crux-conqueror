9bea8b79fe21ecfcd0a206c8b944cf6dbda624f8
Luke <40407039@qub.ac.uk>
2026-04-04 19:50:16 +0100

    updating FoodEntryRepo

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/FoodEntryRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/FoodEntryRepo.java
@@ -1,4 +1,5 @@
 package com.cruxconqueror.crux_conqueror.repository;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.cruxconqueror.crux_conqueror.model.FoodEntry;
@@ -6,5 +7,7 @@ import com.cruxconqueror.crux_conqueror.model.User;
 
 public interface FoodEntryRepo extends JpaRepository<FoodEntry, Long> {
     List<FoodEntry> findByUserOrderByEntryDateTimeDesc(User user);
-    
+    List<FoodEntry> findByUserAndEntryDateTimeBetweenOrderByEntryDateTimeDesc(
+        User user, LocalDateTime start, LocalDateTime end);
+
 }
