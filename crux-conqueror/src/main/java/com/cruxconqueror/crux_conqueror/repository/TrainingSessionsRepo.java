11afc1628e1b40dd93c3f385b248d8d95e8d2a22
Luke <40407039@qub.ac.uk>
2026-04-04 19:51:43 +0100

    updating my Training Sessions Repo

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/TrainingSessionsRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/TrainingSessionsRepo.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cruxconqueror.crux_conqueror.model.TrainingSessions;
 import com.cruxconqueror.crux_conqueror.model.User;
 import java.time.LocalDateTime;
+
 public interface TrainingSessionsRepo extends JpaRepository<TrainingSessions, Long> {
     List<TrainingSessions> findByUserOrderBySessionDateDesc(User user);
     List<TrainingSessions> findByUserAndArchivedFalseOrderBySessionDateDesc(User user);
     List<TrainingSessions> findByUserAndArchivedTrueOrderBySessionDateDesc(User user);
     List<TrainingSessions> findByArchivedFalseAndSessionDateAfter(LocalDateTime since);
+
+    long countByUserAndArchivedFalse(User user);
 }
