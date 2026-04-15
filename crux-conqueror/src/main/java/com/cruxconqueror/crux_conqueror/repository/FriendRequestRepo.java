a8a9346993a9ec0986ddf6f081070504e93c092a
Luke <40407039@qub.ac.uk>
2026-04-15 19:27:31 +0100

    Creating the content for my FriendRequestRepo

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/FriendRequestRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/FriendRequestRepo.java
@@ -1,5 +1,13 @@
 package com.cruxconqueror.crux_conqueror.repository;
-
-public class FriendRequestRepo {
-    
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cruxconqueror.crux_conqueror.model.FriendRequest;
+import com.cruxconqueror.crux_conqueror.model.User;
+public interface FriendRequestRepo extends JpaRepository<FriendRequest, Long> {
+    List<FriendRequest> findByReceiverAndStatusOrderByCreatedAtDesc(User receiver, String status);
+    List<FriendRequest> findBySenderAndStatusOrderByCreatedAtDesc(User sender, String status);
+    List<FriendRequest> findBySenderAndReceiver(User sender, User receiver);
+    List<FriendRequest> findBySenderAndStatusOrReceiverAndStatus(User sender, String senderStatus, User receiver, String receiverStatus);
+    Optional<FriendRequest> findById(Long id);
 }
