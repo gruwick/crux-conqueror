828afa69d0506480ad3286b6d438d5a9ea6d742e
Luke <40407039@qub.ac.uk>
2026-04-17 21:53:02 +0100

    and my Forum favourite repo

---
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/ForumFavouriteRepo.java
@@ -0,0 +1,13 @@
+package com.cruxconqueror.crux_conqueror.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cruxconqueror.crux_conqueror.model.ForumFavourite;
+import com.cruxconqueror.crux_conqueror.model.ForumPost;
+import com.cruxconqueror.crux_conqueror.model.User;
+
+public interface ForumFavouriteRepo extends JpaRepository<ForumFavourite, Long> {
+    Optional<ForumFavourite> findByPostAndUser(ForumPost post, User user);
+    List<ForumFavourite> findByUser(User user);
+}
