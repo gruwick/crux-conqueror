bac6a71125519ee0f7eaecf8e833e04d0aa6c2fd
Luke <40407039@qub.ac.uk>
2026-04-18 01:58:56 +0100

    making its subsequent repo

---
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/ForumCommentRepo.java
@@ -0,0 +1,11 @@
+package com.cruxconqueror.crux_conqueror.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cruxconqueror.crux_conqueror.model.ForumComment;
+import com.cruxconqueror.crux_conqueror.model.ForumPost;
+
+public interface ForumCommentRepo extends JpaRepository<ForumComment, Long> {
+    List<ForumComment> findByPostOrderByCreatedAtAsc(ForumPost post);
+    long countByPost(ForumPost post);
+}
