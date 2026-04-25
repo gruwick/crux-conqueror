ba741431264e9793b7a5c50c8cb3e97153337b35
Luke <40407039@qub.ac.uk>
2026-04-26 00:25:26 +0100

    commenting my security

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/config/SecurityConfiguration.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/config/SecurityConfiguration.java
@@ -10,39 +10,65 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+/**
+ * Main Security Config for the application
+ * 
+ * Sets up the folowwing:
+ * -Password hashing (BCrypt)
+ * -Authentication using custom UserDetailsService
+ * -Route protection for login
+ * -login and out behaviours
+ */
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    //BCrypt used to securely has user passwords before storing
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    //Connects Spring Security to our custom UserDetailService
     @Bean
     public DaoAuthenticationProvider authProvider(UserDetailsService userDetailsService,
             PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        // Uses DbUserDetailsService to load users from database
         provider.setUserDetailsService(userDetailsService);
+
+        // Ensures passwords are checked using BCrypt hashing
         provider.setPasswordEncoder(passwordEncoder);
         return provider;
 
     }
-
+    // Defines the routes that are secured and how login and logout behaves
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                //CSRF disables for simplcity during dev, can be reenabled in production
                 .csrf(csrf -> csrf.disable())
+
+                //Definine public vs private routes
                 .authorizeHttpRequests(auth -> auth
+                        // allows acces to static resources without login
                         .requestMatchers("/css/**", "/images/**", "/js/**, /test/**").permitAll()
+
+                        //allow access to login/register pages
                         .requestMatchers("/login", "/register", "/error").permitAll()
+
+                        //All other pages require authentication
                         .anyRequest().authenticated())
+                
+                //Custom login cnfig
                 .formLogin(form -> form
                         .loginPage("/login")
                         .loginProcessingUrl("/login")
                         .defaultSuccessUrl("/", true)
                         .failureUrl("/login?error")
                         .permitAll())
+
+                //logout behaviour
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login?logout"));
 
