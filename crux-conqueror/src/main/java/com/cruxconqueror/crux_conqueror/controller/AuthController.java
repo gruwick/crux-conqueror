e06cee86b59539c48f7b202ef0d259908886f6a6
Luke <40407039@qub.ac.uk>
2026-04-26 00:41:52 +0100

    comments for authController made

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/controller/AuthController.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/controller/AuthController.java
@@ -2,7 +2,12 @@ package com.cruxconqueror.crux_conqueror.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/**
+ * Controller responsivlle for handling authentication ciews
+ * 
+ * Onlt handles login page
+ * Authentication handle by Spring Security config
+ */
 @Controller
 public class AuthController {
 
