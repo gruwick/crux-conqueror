a091af0bae961ba9fd0f97a6644c7600f116a456
Luke <40407039@qub.ac.uk>
2026-04-26 12:47:30 +0100

    forumCommentsrepo commented

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/ForumCommentRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/ForumCommentRepo.java
@@ -4,9 +4,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.cruxconqueror.crux_conqueror.model.ForumComment;
 import com.cruxconqueror.crux_conqueror.model.ForumPost;
-
+/** Repository for accessing forum comment data
+ * Provides methods for retrieving comments fro a post
+ * Retrives engagement metrics */ 
 public interface ForumCommentRepo extends JpaRepository<ForumComment, Long> {
+    //Return all coments for a post in created by order
     List<ForumComment> findByPostOrderByCreatedAtAsc(ForumPost post);
-
+    //Returns total number of comments per post
     long countByPost(ForumPost post);
 }
