5c8784de6d0af6c1193ee746fd720c6461bf217a
Luke <40407039@qub.ac.uk>
2026-04-26 12:18:47 +0100

    Forum Like commented

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/model/ForumLike.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/model/ForumLike.java
@@ -2,6 +2,10 @@ package com.cruxconqueror.crux_conqueror.model;
 
 import jakarta.persistence.*;
 
+/** Entity representing a like on a forum post
+ * 
+ * Creates a relationship betwen user and a post they have liked
+ */
 @Entity
 @Table(name = "Forum_Likes")
 public class ForumLike {
@@ -9,19 +13,19 @@ public class ForumLike {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Like_ID")
     private Long id;
-
+    //The post that has been liked
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "Post_ID", nullable = false)
     private ForumPost post;
-
+    //User who liked the post
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "User_ID", nullable = false)
 
     private User user;
-
+    //Default constructor required by JPA
     public ForumLike() {
     }
-
+    //Getters and setters
     public Long getId() {
         return id;
     }
