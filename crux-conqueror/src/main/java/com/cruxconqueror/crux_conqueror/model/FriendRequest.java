38c6986e44efaa686cd7ba4e5a6889169666fccc
Luke <40407039@qub.ac.uk>
2026-04-26 12:23:51 +0100

    Friend requests commented

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/model/FriendRequest.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/model/FriendRequest.java
@@ -2,7 +2,12 @@ package com.cruxconqueror.crux_conqueror.model;
 
 import java.time.LocalDateTime;
 import jakarta.persistence.*;
-
+/**
+ * Entity representing a friend request between two users
+ * 
+ * Stores the ender, reciever, request status and timestamp
+ * Allows system to manage social connections
+ */
 @Entity
 @Table(name = "Friend_Requests")
 public class FriendRequest {
@@ -10,24 +15,24 @@ public class FriendRequest {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Request_ID")
     private Long id;
-
+    // The user who sent the friend request
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "Sender_User_ID", nullable = false)
     private User sender;
-
+    //The user recieving the friend request
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "Receiver_User_ID", nullable = false)
     private User receiver;
-
+    //Current status of the request
     @Column(name = "Status", nullable = false, length = 20)
     private String status;
-
+    //Timestamp of the request
     @Column(name = "Created_At", nullable = false)
     private LocalDateTime createdAt;
-
+    //Default contructor required by JPA
     public FriendRequest() {
     }
-
+    //Getters and setters
     public Long getId() {
         return id;
     }
