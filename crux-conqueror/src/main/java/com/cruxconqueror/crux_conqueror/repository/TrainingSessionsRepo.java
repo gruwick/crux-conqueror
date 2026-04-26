6cddaa904573f29db6a42c69301f925e82c0acfb
Luke <40407039@qub.ac.uk>
2026-04-26 13:21:02 +0100

    training sessions repo commented

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/TrainingSessionsRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/TrainingSessionsRepo.java
@@ -6,18 +6,24 @@ import com.cruxconqueror.crux_conqueror.model.TrainingSessions;
 import com.cruxconqueror.crux_conqueror.model.User;
 import java.time.LocalDateTime;
 import java.util.Optional;
-
+/** Repository for accessing training session data
+ * 
+ * Suports retrieving sessionf for users filtering archived data
+ * generating stas for dashboard and leaderboard features
+*/
 public interface TrainingSessionsRepo extends JpaRepository<TrainingSessions, Long> {
+    //returns all sessions for  a user by most recent
     List<TrainingSessions> findByUserOrderBySessionDateDesc(User user);
-
+    //Returns active sessions
     List<TrainingSessions> findByUserAndArchivedFalseOrderBySessionDateDesc(User user);
-
+    //returns archived sessions
     List<TrainingSessions> findByUserAndArchivedTrueOrderBySessionDateDesc(User user);
-
+    //Returns all active sessions for a given date
+    //Used by leaderboard and recent activity calculations
     List<TrainingSessions> findByArchivedFalseAndSessionDateAfter(LocalDateTime since);
-
+    //returns the total number of sessions for a user
     long countByUserAndArchivedFalse(User user);
-
+    //returns most recent session for a user
     Optional<TrainingSessions> findFirstByUserAndArchivedFalseOrderBySessionDateDesc(User user);
 
 }
