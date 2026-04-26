6e9a5e561880c7801ea24ebb35a3d1eea87ead34
Luke <40407039@qub.ac.uk>
2026-04-26 12:16:46 +0100

    ForumFavourite commented

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/model/ForumFavourite.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/model/ForumFavourite.java
@@ -2,6 +2,8 @@ package com.cruxconqueror.crux_conqueror.model;
 
 import jakarta.persistence.*;
 
+//** Entity representing users favoruited forum posts
+// Creates a relationship betwen user and post they have favourited */
 @Entity
 @Table(name = "Forum_Favourites")
 public class ForumFavourite {
@@ -9,18 +11,18 @@ public class ForumFavourite {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Favourite_ID")
     private Long id;
-
+    //The post that has been favourited
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "Post_ID", nullable = false)
     private ForumPost post;
-
+    //The user who favourited
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "User_ID", nullable = false)
     private User user;
-
+    //Default constructor required for JPA
     public ForumFavourite() {
     }
-
+    //Getters and setters
     public Long getId() {
         return id;
     }
