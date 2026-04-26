9cc9e30a809471999ce05e74110447f5533aa334
Luke <40407039@qub.ac.uk>
2026-04-26 12:45:09 +0100

    commenting food entry Repo

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/FoodEntryRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/FoodEntryRepo.java
@@ -6,13 +6,20 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.cruxconqueror.crux_conqueror.model.FoodEntry;
 import com.cruxconqueror.crux_conqueror.model.User;
-
+/** Repository for accessing foodEntry
+ * 
+ * Provides methods for retrieving user nutrition entries
+ * Included date based filtering and recent entries
+ */
 public interface FoodEntryRepo extends JpaRepository<FoodEntry, Long> {
+    //Returns all food entries for user, ordered by most recent
     List<FoodEntry> findByUserOrderByEntryDateTimeDesc(User user);
-
+    /**Returns food entries withing a specified date range
+     * Used for dauly and weekly nutrition calculations
+     */
     List<FoodEntry> findByUserAndEntryDateTimeBetweenOrderByEntryDateTimeDesc(
             User user, LocalDateTime start, LocalDateTime end);
-
+    //Returns the most recent food entry
     Optional<FoodEntry> findFirstByUserOrderByEntryDateTimeDesc(User user);
 
 }
