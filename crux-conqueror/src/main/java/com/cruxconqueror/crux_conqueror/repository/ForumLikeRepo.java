c93ced06e5786c22786927f7806ce24b153f3853
Luke <40407039@qub.ac.uk>
2026-04-26 13:13:00 +0100

    ForumLikeRepo commented

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/ForumLikeRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/ForumLikeRepo.java
@@ -6,11 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cruxconqueror.crux_conqueror.model.ForumLike;
 import com.cruxconqueror.crux_conqueror.model.ForumPost;
 import com.cruxconqueror.crux_conqueror.model.User;
-
+/** Repository for accessing forum like data
+ * suports tracking user likes and calulating post popularity
+ */
 public interface ForumLikeRepo extends JpaRepository<ForumLike, Long> {
+    //Returns a like entry for a given post and user
+    //used to determin if a post has already been liked
     Optional<ForumLike> findByPostAndUser(ForumPost post, User user);
-
+    // returns total number of likes for post
     long countByPost(ForumPost post);
-
+    //returns all likes made by user
     List<ForumLike> findByUser(User user);
 }
