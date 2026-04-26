13b62033dd72c61863538c082541f58822f6dd99
Luke <40407039@qub.ac.uk>
2026-04-26 13:10:02 +0100

    ForumFavouriteRepo commented

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/ForumFavouriteRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/ForumFavouriteRepo.java
@@ -6,9 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cruxconqueror.crux_conqueror.model.ForumFavourite;
 import com.cruxconqueror.crux_conqueror.model.ForumPost;
 import com.cruxconqueror.crux_conqueror.model.User;
-
+/**
+ * Repository for accessing forum favourite data
+ * 
+ * Supports retrieving user favourites and checking if a post has been favourites
+ */
 public interface ForumFavouriteRepo extends JpaRepository<ForumFavourite, Long> {
+    //Returns a favourite entry for a post and user
+    //used to check if a post is already favourited
     Optional<ForumFavourite> findByPostAndUser(ForumPost post, User user);
-
+    //Returns all posts favouirte by a user
     List<ForumFavourite> findByUser(User user);
 }
