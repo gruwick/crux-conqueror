ee074d19aed302eb27f8b025e0d232fedbafa422
Luke <40407039@qub.ac.uk>
2026-04-26 13:18:11 +0100

    FriendRequestRepo commented

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/FriendRequestRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/FriendRequestRepo.java
@@ -5,16 +5,22 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.cruxconqueror.crux_conqueror.model.FriendRequest;
 import com.cruxconqueror.crux_conqueror.model.User;
-
+/** 
+ * Repository for friend request data
+ * 
+ * Supports recieving requests by sender, reciever and status
+ * Enabling features such as pending requests and friend filtering
+ */
 public interface FriendRequestRepo extends JpaRepository<FriendRequest, Long> {
+    //Returns all requests recived by a user
     List<FriendRequest> findByReceiverAndStatusOrderByCreatedAtDesc(User receiver, String status);
-
+    //returns all requests sent by a user
     List<FriendRequest> findBySenderAndStatusOrderByCreatedAtDesc(User sender, String status);
-
+    //Check to see if a relationship already exists
     List<FriendRequest> findBySenderAndReceiver(User sender, User receiver);
-
+    //Retrieving all active or accepted relationships
     List<FriendRequest> findBySenderAndStatusOrReceiverAndStatus(User sender, String senderStatus, User receiver,
             String receiverStatus);
-
+    // recieves friend rewuest by id
     Optional<FriendRequest> findById(Long id);
 }
