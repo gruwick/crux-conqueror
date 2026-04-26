3fb5d2561818f37587377ac4af32ec5c4fe0bbd0
Luke <40407039@qub.ac.uk>
2026-04-26 13:23:31 +0100

    user repo commented

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/UserRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/UserRepo.java
@@ -4,13 +4,22 @@ import com.cruxconqueror.crux_conqueror.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.List;
-
+/** Repository for accessing user data
+ * 
+ * Supports authentication, validation checks
+ * and user search functionality
+ */
 public interface UserRepo extends JpaRepository<User, Long> {
+    //return user by username
+    //used for authentication
     Optional<User> findByUsername(String username);
-
+    //Searches for partial username match
+    //used for searching or adding friends
     List<User> findByUsernameContainingIgnoreCase(String username);
-
+    //Checks if a username exists
+    //used during registration validation
     boolean existsByUsername(String username);
-
+    // checks if an email exits
+    //used during registration validation
     boolean existsByEmail(String email);
 }
