4134d3bff445376691afaa10465f947417fea382
Luke <40407039@qub.ac.uk>
2026-04-26 13:15:04 +0100

    ForumPostRepo Commented

--- crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/ForumPostRepo.java
+++ crux-conqueror/src/main/java/com/cruxconqueror/crux_conqueror/repository/ForumPostRepo.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cruxconqueror.crux_conqueror.model.ForumPost;
-
+/** Repository for accessing forum post data
+ * 
+ * Provides methods for retriving posts for community feature
+ * incldues global and user specific views
+ */
 public interface ForumPostRepo extends JpaRepository<ForumPost, Long> {
+    // returns all posts ordered by most recent first
     List<ForumPost> findAllByOrderByCreatedAtDesc();
-
+    //returns posts created by specific set of users
+    //used for friends only feed
     List<ForumPost> findByUserUsernameInOrderByCreatedAtDesc(List<String> usernames);
 }
